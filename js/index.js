e22a66e76a89b2d8f564f701552dbf87e40fc2bd
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-10 11:42:51 +0000

    test

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
 window.onload = function () {
-  //document.body.style.removeProperty('background-image'); //why????????
+  document.body.style.removeProperty('background-image'); //why????????
   let pichai = new PichaiUX();
   pichai.initialize();
 }
