bf65ac0bf7a78197d5a0ad65a37b75cd7f0319f5
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-11 07:25:18 +0000

    test

--- js/setup.js
+++ js/setup.js
@@ -3,4 +3,4 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 let pichai = new PichaiUX();
 pichai.initialize();
 
-document.body.style.removeProperty('background-image'); //do it again to remove bg image from html so it doesn't look wierd
+document.body.style.backgroundImage = 'initial';
