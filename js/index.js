e0b75632a0129ec1c1202d2b2a6eb314ee015be7
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-11 07:21:31 +0000

    fixed a loading issue

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
 let pichai = new PichaiUX();
 pichai.initialize();
-alert()
 
 let hide = ['Settings', 'Discord Server', 'Support', 'Donate', 'About'];
 let ignore = ['Recent', 'New', 'All', 'Home'];
--- js/setup.js
+++ js/setup.js
@@ -1,8 +1,6 @@
 import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
-window.onload = function () {
-  let pichai = new PichaiUX();
-  pichai.initialize();
+let pichai = new PichaiUX();
+pichai.initialize();
 
-  document.body.style.removeProperty('background-image'); //do it again to remove bg image from html so it doesn't look wierd
-}
+document.body.style.removeProperty('background-image'); //do it again to remove bg image from html so it doesn't look wierd
