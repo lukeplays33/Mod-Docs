172a6e9aa2847052fb832343b7b667fee283cf73
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-13 14:23:22 +0000

    typo

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,4 @@
 document.getElementById('settingsListView').addEventListener('itemSelected', function (e) {
-    console.log(e.detail.index)
     if(e.detail.index == 5) {
         window.open('https://buymeacoffee.com/sketchdonate');
     }
