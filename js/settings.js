df034156f86a2b27848025b12b438a124041aa6e
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-13 14:21:05 +0000

    test

--- js/settings.js
+++ js/settings.js
@@ -1,4 +1,5 @@
 document.getElementById('settingsListView').addEventListener('itemSelected', function (e) {
+    console.log(e.detail.index)
     if(e.detail.index == 5) {
         window.open('https://buymeacoffee.com/sketchdonate');
     }
