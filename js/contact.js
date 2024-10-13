5a3ce3d82cd58c5e3a2fb468109482d286a0bd3d
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-13 10:06:23 +0000

    test

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,8 @@
 document.getElementById('support').addEventListener('itemSelected', function (e) {
     if(e.detail.index == 0) {
         window.open('https://discord.gg/2tJvuH73Kq');
+    } else if(e.detail.index == 1) {
+        parent.document.title = 'FAQ';
+        parent.document.getElementById('pages').src = '../docPgaes/faq.html';
     }
 });
