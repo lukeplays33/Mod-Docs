2a080ebefe1cd25c2788c5b2a7f93b3f97a30184
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-13 14:14:54 +0000

    test

---
+++ js/settings.js
@@ -0,0 +1,5 @@
+document.getElementById('support').addEventListener('itemSelected', function (e) {
+    if(e.detail.index == 5) {
+        window.open('https://buymeacoffee.com/sketchdonate');
+    }
+});
