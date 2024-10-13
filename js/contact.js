0ce1b988f7e952372dcb9f8d011f6a20a130ea6e
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-13 09:58:31 +0000

    wrong event name

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,4 @@
-document.getElementById('support').addEventListener('click', function (e) {
+document.getElementById('support').addEventListener('itemSelected', function (e) {
     if(e.index == 0) {
         window.open('https://discord.gg/2tJvuH73Kq');
     }
