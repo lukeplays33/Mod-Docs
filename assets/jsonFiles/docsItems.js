c1e3533f0acdfad9947fd8b6ef53fa59eb1646e7
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-14 12:47:21 +0000

    test

--- assets/jsonFiles/docsItems.js
+++ assets/jsonFiles/docsItems.js
@@ -1,12 +1,18 @@
 let docsItemsJson = {
-    "Thunkable": {},
-    "Pichai_UX": {},
-    "Lucy": {},
-    "Gryffindor": {},
-    "NexOS": {},
-    "Stereo": {},
-    "Debugger": {},
-    "Mod_Docs!": {}
-  }
+  "Thunkable": {
+    "Keyboard_Events": {},
+    "Mouse_Events": {},
+    "Theme_Swapper": {},
+    "ASD": {},
+    "Console": {}
+  },
+  "Pichai_UX": {},
+  "Lucy": {},
+  "Gryffindor": {},
+  "NexOS": {},
+  "Stereo": {},
+  "Debugger": {},
+  "Mod_Docs!": {}
+}
 
-  export { docsItemsJson }
+export { docsItemsJson }
--- js/index.js
+++ js/index.js
@@ -10,24 +10,21 @@ pichai.createSimpleDrawer(drawer, 'auto');
 
 let tutorials = document.getElementById('tutorials');
 
-let items = document.getElementsByTagName('A');
-let i;
-for (i of items) {
-  i.addEventListener('click', function () {
-
-
-    document.getElementById('pages').src = this.href;
-
-    document.title = `Mod Docs! - ${this.innerHTML}`;
-  });
-}
-
 document.getElementById('HomeButton').click();
 
 for(i of Object.keys(docsItemsJson)) {
   let a = document.createElement('a');
   a.innerHTML = String(i).replaceAll('_', '');
-  a.href = '';
+  a.href = '../Mod-Docs/docPages/docs.html';
 
   drawer.insertBefore(a, tutorials);
+}
+
+let items = document.getElementsByTagName('A');
+let i;
+for (i of items) {
+  i.addEventListener('click', function () {
+    document.getElementById('pages').src = this.href;
+    document.title = `Mod Docs! - ${this.innerHTML}`;
+  });
 }
