45319466aa9f64ac21a1d4a804f0bb8873820bb6
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-14 12:05:47 +0000

    test

---
+++ assets/jsonFiles/docsItems.js
@@ -0,0 +1,12 @@
+let docsItemsJson = {
+    "Thunkable": {},
+    "Pichai-UX": {},
+    "Lucy": {},
+    "Gryffindor": {},
+    "NexOS": {},
+    "Stereo": {},
+    "Debugger": {},
+    "Mod_Docs!": {}
+  }
+
+  export { docsItemsJson }
--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
 
 import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
-import { waitForElement } from '../js/utils.js';
+import { docsItemsJson } from '../assets/jsonFiles/docsItems';
 
 let pichai = new PichaiUX();
 pichai.initialize();
 
 let drawer = document.getElementById('draawer');
 pichai.createSimpleDrawer(drawer, 'auto');
-
-let headerGroups = document.getElementsByClassName('header');
 
 let hide = ['Settings', 'Support', 'Donate', 'About'];
 let ignore = ['Recent', 'New', 'All', 'Home'];
@@ -17,16 +15,7 @@ let items = document.getElementsByTagName('A');
 let i;
 for (i of items) {
   i.addEventListener('click', function () {
-    if (hide.includes(this.innerHTML)) {
-      document.getElementById('docs').style.display = 'none';
-      document.getElementById('bottom_nav').style.display = 'none';
-    } else if (ignore.includes(this.innerHTML)) {
-      document.getElementById('docs').style.display = 'none';
-      document.getElementById('bottom_nav').style.display = 'flex';
-    } else {
-      document.getElementById('docs').style.display = 'flex';
-      document.getElementById('bottom_nav').style.display = 'none';
-    }
+
 
     document.getElementById('pages').src = this.href;
 
@@ -35,3 +24,7 @@ for (i of items) {
 }
 
 document.getElementById('HomeButton').click();
+
+for(i of docsItemsJson.keys) {
+
+}
