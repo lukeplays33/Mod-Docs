564cdb59cd2a6dd934df20fdfec402578a5d5202
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-15 12:14:15 +0000

    test

--- js/docItems.js
+++ js/docItems.js
@@ -4,4 +4,4 @@ let description = document.getElementById('description');
 let items = document.getElementById('items');
 
 description.children[1].innerHTML = docsItemsJson[window.sessionStorage.getItem('docItem')].description;
-items.listItems = Object.keys(docsItemsJson[window.sessionStorage.getItem('docItem')].items);
+items.listItems = Object.keys(docsItemsJson[window.sessionStorage.getItem('docItem')].items)
