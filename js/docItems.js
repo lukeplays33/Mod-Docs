264098e04b0ffc02d154b41a5e9fd5ad7475376e
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-15 12:03:02 +0000

    added possibility to change listItems from js

--- js/docItems.js
+++ js/docItems.js
@@ -1,5 +1,7 @@
 import { docsItemsJson } from '../assets/jsonFiles/docsItems.js';
 
 let description = document.getElementById('description');
+let items = document.getElementById('items');
 
 description.children[1].innerHTML = docsItemsJson[window.sessionStorage.getItem('docItem')].description;
+items.listItems = Object.keys(docsItemsJson[window.sessionStorage.getItem('docItem')].items);
