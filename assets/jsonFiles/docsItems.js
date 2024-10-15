d9cf48d432e2d834e726086eaa0651e709fa2753
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-15 08:59:56 +0000

    test

--- assets/jsonFiles/docsItems.js
+++ assets/jsonFiles/docsItems.js
@@ -7,7 +7,7 @@ let docsItemsJson = {
   "Debugger": {},
   "Mod_Docs!": {},
   "Thunkable": {
-    description: 'Modules are a way that Thunkable users can create more functions for their app. it allows them to developer easier and faster. \n Below is a list of Modules we have developed for Thunkable.',
+    description: 'Modules are a way that Thunkable users can create more functions for their app. it allows them to develop easier and faster. <br> Below is a list of Modules we have developed for Thunkable.',
     items: {
       "Keyboard_Events": {},
       "Mouse_Events": {},
