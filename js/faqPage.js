2ca486b45bb5f800a09ebd7bd553739322e2c9e7
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-15 13:54:26 +0000

    test

--- js/faqPage.js
+++ js/faqPage.js
@@ -1,5 +1,14 @@
 import { faqItems } from "../assets/jsonFiles/faq.js";
 
-let items = document.getElementById('faqs');
+let faqList = document.getElementById('faqs');
 
-items.listItems = Object.keys(faqItems);
+faqList.listItems = Object.keys(faqItems);
+
+faqList.addEventListener('itemSelected', function (e) {
+    faqList.getElementsByClassName('des')[0].remove();
+
+    let des = document.createElement('li');
+    des = faqItems[e.target.item];
+
+    faqList.children[e.target.index].after(des);
+});
