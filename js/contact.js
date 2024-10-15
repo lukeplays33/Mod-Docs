faf9a5cfbf8b6e78449d080568bb4751dd730d58
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-15 13:59:07 +0000

    test

--- js/contact.js
+++ js/contact.js
@@ -2,7 +2,7 @@ document.getElementById('support').addEventListener('itemSelected', function (e)
     if(e.detail.index == 0) {
         window.open('https://discord.gg/2tJvuH73Kq');
     } else if(e.detail.index == 1) {
-        parent.document.title = 'FAQ';
+        parent.document.title = 'Mod Docs - FAQ';
         parent.document.getElementById('pages').src = '../Mod-Docs/docPages/faq.html';
     }
 });
