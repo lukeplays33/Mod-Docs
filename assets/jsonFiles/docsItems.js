28e735bee9c8b398bdd59be9ba822e754485e979
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-15 12:24:23 +0000

    added reszie and orientation change events

--- assets/jsonFiles/docsItems.js
+++ assets/jsonFiles/docsItems.js
@@ -7,10 +7,10 @@ let docsItemsJson = {
   "Debugger": {},
   "Mod_Docs!": {},
   "Thunkable": {
-    description: 'Modules are a way that Thunkable users can create more functions for their app. it allows them to develop easier and faster. <br> Below is a list of Modules we have developed for Thunkable.',
+    description: 'Modules are a way that allows Thunkable users can create more functions for their app. it allows them to develop easier and faster. <br> Below is a list of Modules we have developed for Thunkable.',
     items: {
       "Keyboard_Events": {},
-      "Mouse_Events": {},
+      "Screen_Events": {},
       "Theme_Swapper": {},
       "ASD": {},
       "Console": {}
