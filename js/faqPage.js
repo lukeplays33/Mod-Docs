97d4e414d3cc4fa222b8f7b4988b8d0e1f448510
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-16 08:13:18 +0000

    test

--- js/faqPage.js
+++ js/faqPage.js
@@ -5,14 +5,12 @@ let faqList = document.getElementById('faqs');
 faqList.listItems = Object.keys(faqItems);
 
 faqList.addEventListener('itemSelected', function (e) {
-    console.log(e)
     try {
         faqList.getElementsByClassName('des')[0].remove();
     } catch (e) { }
 
     let des = document.createElement('li');
-    des = faqItems[e.target.value];
+    des = faqItems[e.detail.value];
 
-    console.log(e.target.value);
-    document.getElementById(e.target.value).after(des);
+    document.getElementById(e.detail.value).after(des);
 });
