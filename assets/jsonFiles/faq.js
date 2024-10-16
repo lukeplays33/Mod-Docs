459540bc97f882af3eff853f74378c7d09b92d6f
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-16 08:19:08 +0000

    test

--- assets/jsonFiles/faq.js
+++ assets/jsonFiles/faq.js
@@ -1,6 +1,7 @@
 let faqItems = {
     "Why do we barely use Logic Modules for Thunkable?": "This is because Modules allow us to interact with the screen, most of the time we need access to the screen for the modules to work even if the are not visible.",
-    "Why do we have a requestFocus() function in Thunkable?": "To have JavaScript events work such as a key event the user needs to have focused the iframe it is in, this function allows that after the focus was lost the developer can request it back to the iframe to capture events."
+    "Why do we have a requestFocus() function in Thunkable?": "To have JavaScript events work such as a key event the user needs to have focused the iframe it is in, this function allows that after the focus was lost the developer can request it back to the iframe to capture events.",
+    
 }
   
   export { faqItems }
