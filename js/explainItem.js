3ebf176e39aa0fd6e95e6695ffe97371e0619e8e
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-17 11:48:03 +0000

    im stupid

--- js/explainItem.js
+++ js/explainItem.js
@@ -3,7 +3,6 @@ import { docsItemsJson } from '../assets/jsonFiles/docsItems.js';
 let description = document.getElementById('description');
 let items = document.getElementById('items');
 
-console.log(docsItemsJson[window.sessionStorage.getItem('docItem')][window.sessionStorage.getItem('explainItem')].about.description)
 description.children[1].innerHTML = docsItemsJson[window.sessionStorage.getItem('docItem')][window.sessionStorage.getItem('explainItem')].about.description;
 items.listItems = Object.keys(docsItemsJson[window.sessionStorage.getItem('docItem')][window.sessionStorage.getItem('explainItem')].contents);
 
