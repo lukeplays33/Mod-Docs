9ea8439375ceb22c056d9d3ad4f36271c9e5814a
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-17 11:07:22 +0000

    rest

--- assets/jsonFiles/troubleshootings.js
+++ assets/jsonFiles/troubleshootings.js
@@ -1,5 +1,6 @@
 let troubleShooting = {
-    "No events captured": "Ensure that your The modules are focused to capture events, this can be done by using the requestFocus() function."
+    "No events captured": "Ensure that your The modules are focused to capture events, this can be done by using the requestFocus() function.",
+    "I'm just lost!": "If you still need help or don't understand something in the docs, then you can join the Community and ask a question in the right forums to get extra help."
 }
 
 export { troubleShooting }
