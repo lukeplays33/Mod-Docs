9641821c6acc2a43554ffa98fee7a147b8eb1e83
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-17 11:14:42 +0000

    test

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,8 @@
-import { settings } from "../assets/jsonFiles/supportSettings.js";
-console.log(document.getElementById('support'))
+//import { settings } from "../assets/jsonFiles/supportSettings.js";
 
 document.getElementById('support').addEventListener('itemSelected', function (e) {
     if(e.detail.index == 0) {
-        window.open(settings.communityServer);
+        //window.open(settings.communityServer);
     } else if(e.detail.index == 1) {
         parent.document.title = 'Mod Docs - FAQ';
         parent.document.getElementById('pages').src = '../Mod-Docs/docPages/faq.html';
