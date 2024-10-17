88942d53ccbeef4236d0ff312a3409abf0e4773e
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-17 11:54:28 +0000

    test

--- js/explainItem.js
+++ js/explainItem.js
@@ -1,12 +1,7 @@
 import { docsItemsJson } from '../assets/jsonFiles/docsItems.js';
 
 let description = document.getElementById('description');
-let items = document.getElementById('items');
+let items = document.getElementById('docs');
 
 description.children[1].innerHTML = docsItemsJson[window.sessionStorage.getItem('docItem')].items[window.sessionStorage.getItem('explainItem')].about.description;
 items.listItems = Object.keys(docsItemsJson[window.sessionStorage.getItem('docItem')].items[window.sessionStorage.getItem('explainItem')].contents);
-
-items.addEventListener('itemSelected', function (e) {
-    parent.document.title = 'Mod Docs - ' + e.detail.value;
-    parent.document.getElementById('pages').src = '../Mod-Docs/docPages/explainItem.html';
-});
