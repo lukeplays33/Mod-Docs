a64521d20ce7768a6d748312304f31e6bc903dc8
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-17 11:49:27 +0000

    test

--- js/explainItem.js
+++ js/explainItem.js
@@ -3,6 +3,7 @@ import { docsItemsJson } from '../assets/jsonFiles/docsItems.js';
 let description = document.getElementById('description');
 let items = document.getElementById('items');
 
+console.log(docsItemsJson[window.sessionStorage.getItem('docItem')][window.sessionStorage.getItem('explainItem')])
 description.children[1].innerHTML = docsItemsJson[window.sessionStorage.getItem('docItem')][window.sessionStorage.getItem('explainItem')].about.description;
 items.listItems = Object.keys(docsItemsJson[window.sessionStorage.getItem('docItem')][window.sessionStorage.getItem('explainItem')].contents);
 
