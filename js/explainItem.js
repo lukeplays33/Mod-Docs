3114acde61df030e22df09460df273d544c4ca11
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-17 13:18:11 +0000

    test

--- js/explainItem.js
+++ js/explainItem.js
@@ -3,15 +3,15 @@ import { docsItemsJson } from '../assets/jsonFiles/docsItems.js';
 let description = document.getElementById('description');
 let items = document.getElementById('docs');
 
+let concatList = ['Information'];
+
 description.children[1].innerHTML = docsItemsJson[window.sessionStorage.getItem('docItem')].items[window.sessionStorage.getItem('explainItem')].about.description;
 items.listItems = Object.keys(docsItemsJson[window.sessionStorage.getItem('docItem')].items[window.sessionStorage.getItem('explainItem')].contents);
 
 if(docsItemsJson[window.sessionStorage.getItem('docItem')].items[window.sessionStorage.getItem('explainItem')].about.demo) {
-    items.listItems.unshift('Demo');
+    concatList.unshift('Demo');
     
 }
 
-items.listItems.unshift('Information');
-
-//items.listItems = Object.keys(docsItemsJson[window.sessionStorage.getItem('docItem')].items[window.sessionStorage.getItem('explainItem')].contents).concat(concatList);
+items.listItems = Object.keys(docsItemsJson[window.sessionStorage.getItem('docItem')].items[window.sessionStorage.getItem('explainItem')].contents).concat(concatList);
 items.children[0].click();
