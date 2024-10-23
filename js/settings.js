0c41ed30dc9d6b47656a31ee2685bbe8abc9bda4
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-23 10:02:47 +0000

    test

--- js/settings.js
+++ js/settings.js
@@ -3,6 +3,7 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 let pichai = new PichaiUX();
 
 document.getElementById('settingsListView').addEventListener('itemSelected', function (e) {
+    alert(e.detail.index)
     if (e.detail.index == 2) {
         window.open('https://buymeacoffee.com/sketchdonate');
     }
--- js/setup.js
+++ js/setup.js
@@ -3,6 +3,4 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 let pichai = new PichaiUX();
 pichai.initialize();
 
-let interval = window.setInterval( async () => {
-    document.body.style.backgroundImage = 'none';
-}, 500);
+document.body.style.backgroundImage = 'none';
