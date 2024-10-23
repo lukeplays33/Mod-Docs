ea25661d5ed7661458e1954b53523ab645ecd427
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-23 09:06:27 +0000

    test

--- js/setup.js
+++ js/setup.js
@@ -3,4 +3,6 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 let pichai = new PichaiUX();
 pichai.initialize();
 
-document.body.style.backgroundImage = 'initial';
+let interval = window.setInterval( async () => {
+    document.body.style.backgroundImage = 'initial';
+}, 500);
