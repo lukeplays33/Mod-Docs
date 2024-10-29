77ea5beb941b712ff760a7e65f1f31102021534e
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-10-29 08:34:31 +0000

    test

--- js/settings.js
+++ js/settings.js
@@ -3,7 +3,9 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 let pichai = new PichaiUX();
 
 document.getElementById('settingsListView').addEventListener('itemSelected', function (e) {
-    if (e.detail.index == 2) {
+    if (e.detail.index == 1) {
+        window.open('../Mod-Docs/docPages/about.html');
+    } else if (e.detail.index == 2) {
         window.open('https://buymeacoffee.com/sketchdonate');
     }
 });
