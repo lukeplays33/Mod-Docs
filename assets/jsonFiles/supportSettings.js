d6cf3dc449ac3f5d5b69a17b958177f9cdb860a9
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-11-08 09:22:49 +0000

    test

--- assets/jsonFiles/supportSettings.js
+++ assets/jsonFiles/supportSettings.js
@@ -3,6 +3,14 @@ let settings = {
     faq: true,
     troubleShooting: true,
     privacyPolicy: true,
+    about: {
+        url: 'https://lukeplays33.github.io/The-Magic-Garden/pages/about/moddocs.html',
+        message:'What is Mod Docs?'
+    },
+    donations: {
+        url: 'https://buymeacoffee.com/sketchdonate',
+        message:'Donate to Mod Docs!'
+    }
 }
 
 export { settings }
--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,11 @@
 import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
+import { settings } from '../assets/jsonFiles/supportSettings.js';
+
 let pichai = new PichaiUX();
+
+let about = document.getElementById('What is Mod Docs?');
+about.innerText = settings.about.message;
 
 document.getElementById('settingsListView').addEventListener('itemSelected', function (e) {
     if (e.detail.index == 1) {
