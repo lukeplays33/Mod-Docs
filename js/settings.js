45517cf26c4f8dd6d9707715c3789dc73367073d
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-11-08 09:26:59 +0000

    test

--- js/settings.js
+++ js/settings.js
@@ -2,10 +2,10 @@ import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
 import { settings } from '../assets/jsonFiles/supportSettings.js';
 
-let pichai = new PichaiUX();
-
 let about = document.getElementById('What is Mod Docs?');
 about.innerText = settings.about.message;
+
+let pichai = new PichaiUX();
 
 document.getElementById('settingsListView').addEventListener('itemSelected', function (e) {
     if (e.detail.index == 1) {
