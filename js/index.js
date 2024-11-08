849c2ce26bd7591e0504fa8e9b2ac6f2433c1b89
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-11-08 09:17:00 +0000

    test

--- js/index.js
+++ js/index.js
@@ -29,10 +29,10 @@ for (i of items) {
   i.class = '';
 
   i.addEventListener('click', function (e) {
-    if (i.id == 'promo') {
+    if (this.id == 'promo') {
       e.preventDefault();
       e.stopPropagation();
-      
+
       window.open('https://lukeplays33.github.io/The-Magic-Garden/pages/about/moddocs.html');
     } else {
       this.parentNode.getElementsByClassName('current')[0].classList.remove('current');
