3903b898d9077fd93e080e4b2f5b77b1e2526723
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-11-11 10:49:57 +0000

    test

--- js/faqPage.js
+++ js/faqPage.js
@@ -7,11 +7,10 @@ let faqList = document.getElementById('faqs');
 let json = String(window.parent.document.title).includes('FAQ') ? faqItems :
     String(window.parent.document.title).includes('Notes') ? docsItemsJson[window.sessionStorage.getItem('docItem')].items[window.sessionStorage.getItem('explainItem')].about.notes : troubleShooting;
 
-let des = '';
-
 faqList.listItems = Object.keys(json);
 
 faqList.addEventListener('itemSelected', function (e) {
+    let des = '';
     let selectedItem = document.getElementById(e.detail.value);
 
     try {
