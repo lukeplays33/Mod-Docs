8ae8465723ccaf8262e7ada7be42dbda4009f22a
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-11-11 11:14:08 +0000

    test

--- js/faqPage.js
+++ js/faqPage.js
@@ -8,7 +8,7 @@ let json = String(window.parent.document.title).includes('FAQ') ? faqItems :
     String(window.parent.document.title).includes('Notes') ? docsItemsJson[window.sessionStorage.getItem('docItem')].items[window.sessionStorage.getItem('explainItem')].about.notes : troubleShooting;
 
 let des;
-    
+
 faqList.listItems = Object.keys(json);
 
 faqList.addEventListener('itemSelected', function (e) {
@@ -19,15 +19,9 @@ faqList.addEventListener('itemSelected', function (e) {
         des.remove();
     } catch (e) { }
 
-    alert(selectedItem.open)
-    if(selectedItem.open) {
+    des = document.createElement('li');
+    des.classList.add('des');
+    des.innerHTML = json[e.detail.value];
 
-    } else {
-        des = document.createElement('li');
-        des.classList.add('des');
-        des.innerHTML = json[e.detail.value];
-    
-        selectedItem.after(des);
-        selectedItem.open = true;
-    }
+    selectedItem.after(des);
 });
