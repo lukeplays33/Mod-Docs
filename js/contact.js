2d883568de34a1841a28c3f5b2864595ce8bd0ce
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-11-18 12:25:47 +0000

    removed and moved any settings related items in favour of the new quick settings tile

--- js/contact.js
+++ js/contact.js
@@ -1,41 +1,59 @@
 import { settings } from "../assets/jsonFiles/supportSettings.js";
 
 let blog = document.getElementById('Blog');
+let about = document.getElementById('What is Mod Docs?');
+let donate = document.getElementById('Donate to Mod Docs!');
 
-if(!settings.hasOwnProperty('blog')) {
+if (!settings.hasOwnProperty('blog')) {
     blog.remove();
 } else {
-    blog.innerHTML = blog.innerHTML.replace('Blog',settings.blog.message);
+    blog.innerHTML = blog.innerHTML.replace('Blog', settings.blog.message);
 }
 
-if(!settings.faq) {
+if (!settings.hasOwnProperty('about')) {
+    about.remove();
+} else {
+    about.innerHTML = about.innerHTML.replace('What is Mod Docs?', settings.about.message);
+}
+
+if (!settings.hasOwnProperty('donations')) {
+    donate.remove();
+} else {
+    donate.innerHTML = donate.innerHTML.replace('What is Mod Docs?', settings.donations.message);
+}
+
+if (!settings.faq) {
     document.getElementById('FAQ').remove();
 }
 
-if(!settings.troubleShooting) {
+if (!settings.troubleShooting) {
     document.getElementById('Troubleshooting').remove();
 }
 
-if(!settings.privacyPolicy) {
+if (!settings.privacyPolicy) {
     document.getElementById('Privacy policy').remove();
 }
 
-if(!settings.hasOwnProperty('communityServer')) {
+if (!settings.hasOwnProperty('communityServer')) {
     document.getElementById('Community').remove();
 }
 
 document.getElementById('support').addEventListener('itemSelected', function (e) {
-    if(e.detail.value == 'Community') {
+    if (e.detail.value == 'Community') {
         window.open(settings.communityServer);
-    } else if(e.detail.value == 'FAQ') {
+    } else if (e.detail.value == 'FAQ') {
         parent.document.title = 'Mod Docs - FAQ';
         parent.document.getElementById('pages').src = '../Mod-Docs/docPages/faq.html';
-    }  else if(e.detail.value == 'Troubleshooting') {
+    } else if (e.detail.value == 'Troubleshooting') {
         parent.document.title = 'Mod Docs - Troubleshooting';
         parent.document.getElementById('pages').src = '../Mod-Docs/docPages/faq.html';
-    }   else if(e.detail.value == 'Privacy policy') {
+    } else if (e.detail.value == 'Privacy policy') {
         window.open(settings.privacyPolicy);
-    }    else if(e.detail.value == 'Blog') {
+    } else if (e.detail.value == 'Blog') {
         window.open(settings.blog.url);
+    } else if (e.detail.value == settings.about.message) {
+        window.open(settings.about.url);
+    } else if (e.detail.value == settings.donations.message) {
+        window.open(settings.donations.url);
     }
 });
