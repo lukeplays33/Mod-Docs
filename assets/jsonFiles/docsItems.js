3c549cc026a3c536747a729811da6695e6534190
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-11-20 12:48:07 +0000

    test

--- assets/jsonFiles/docsItems.js
+++ assets/jsonFiles/docsItems.js
@@ -98,6 +98,108 @@ let docsItemsJson = {
             type: 'Event', // boolean,int, string, function, class etc or event if it's a event or property for property of elements etc
             displayFile: '../assets/images/docs/Event: keyPress.png', // the file that is displayed all together with the info, it can be an image, video, a code file or a interactive demo that let's user see what happens when a variable it's value is changed for example.
           },
+
+          keyDown: {
+            description: 'The key down event fires when a key is down on the keyboard.',
+            variables: { //the input or outputs it can have, the types they return or accept such as a string or number
+
+              keyName: {
+                readOnly: true, // default for events, cannot be false for events
+
+                defaultValue: '',
+                valueOptions: [], // an array of options telling the developer wich choices are accepted by the input
+
+                returnTypes: ['string'],
+                inputTypes: [],  // cannot have any value when in events since they cant accept input values
+
+                description: 'The name of the key that is pressed.',
+
+                accessCode: "", //what the user can type to access the variable; e.g. event.detail.keyCode
+              },
+
+              keyCode: {
+                readOnly: true, // default for events, cannot be false for events
+
+                defaultValue: '',
+                valueOptions: [], // an array of options telling the developer wich choices are accepted by the input
+
+                returnTypes: ['number'],
+                inputTypes: [],
+
+                description: 'The code of the key that is pressed.',
+
+                accessCode: "", //what the user can type to access the variable; e.g. event.detail.keyCode
+              },
+
+              heldDown: {
+                readOnly: true, // default for events, cannot be false for events
+
+                defaultValue: '',
+                valueOptions: [], // an array of options telling the developer wich choices are accepted by the input
+
+                returnTypes: ['boolean'],
+                inputTypes: [],
+
+                description: 'Returns true if the key is held down for long periods of time.',
+
+                accessCode: "", //what the user can type to access the variable; e.g. event.detail.keyCode
+              },
+            },
+            
+            type: 'Event', // boolean,int, string, function, class etc or event if it's a event or property for property of elements etc
+            displayFile: '../assets/images/docs/Event: keyDown.png', // the file that is displayed all together with the info, it can be an image, video, a code file or a interactive demo that let's user see what happens when a variable it's value is changed for example.
+          },
+
+          keyUp: {
+            description: 'The key pressed event fires when a key is released on the keyboard.',
+            variables: { //the input or outputs it can have, the types they return or accept such as a string or number
+
+              keyName: {
+                readOnly: true, // default for events, cannot be false for events
+
+                defaultValue: '',
+                valueOptions: [], // an array of options telling the developer wich choices are accepted by the input
+
+                returnTypes: ['string'],
+                inputTypes: [],  // cannot have any value when in events since they cant accept input values
+
+                description: 'The name of the key that is pressed.',
+
+                accessCode: "", //what the user can type to access the variable; e.g. event.detail.keyCode
+              },
+
+              keyCode: {
+                readOnly: true, // default for events, cannot be false for events
+
+                defaultValue: '',
+                valueOptions: [], // an array of options telling the developer wich choices are accepted by the input
+
+                returnTypes: ['number'],
+                inputTypes: [],
+
+                description: 'The code of the key that is pressed.',
+
+                accessCode: "", //what the user can type to access the variable; e.g. event.detail.keyCode
+              },
+
+              heldDown: {
+                readOnly: true, // default for events, cannot be false for events
+
+                defaultValue: '',
+                valueOptions: [], // an array of options telling the developer wich choices are accepted by the input
+
+                returnTypes: ['boolean'],
+                inputTypes: [],
+
+                description: 'Returns true if the key is held down for long periods of time.',
+
+                accessCode: "", //what the user can type to access the variable; e.g. event.detail.keyCode
+              },
+            },
+            
+            type: 'Event', // boolean,int, string, function, class etc or event if it's a event or property for property of elements etc
+            displayFile: '../assets/images/docs/Event: keyUp.png', // the file that is displayed all together with the info, it can be an image, video, a code file or a interactive demo that let's user see what happens when a variable it's value is changed for example.
+          },
         },
         //quickLinks: [], //e.g. for quickly navigating to a tutorial
       },
