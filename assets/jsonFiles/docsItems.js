f31bde3d46f699027cf997473dd2b579d7330561
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-12-04 10:39:24 +0000

    test

--- assets/jsonFiles/docsItems.js
+++ assets/jsonFiles/docsItems.js
@@ -54,7 +54,7 @@ let docsItemsJson = {
             },
 
             controls: { // tells the docs page to display controls for displayFile
-              fullscreen:false,
+              fullscreen:true,
               play:false,
               console:false,
               viewCode:false,
