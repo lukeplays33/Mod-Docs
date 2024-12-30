9c67d28b543a1b66d5931283aac15d9354ed91a8
Sketch <54947833+lukeplays33@users.noreply.github.com>
2024-12-30 16:31:49 +0000

    added a troubleshooting item explaining what to do with bugs or ideas

--- assets/jsonFiles/troubleshootings.js
+++ assets/jsonFiles/troubleshootings.js
@@ -1,6 +1,7 @@
 let troubleShooting = {
     "No events captured.": "Ensure that the module is focused so you can capture events, this can be done by using the requestFocus() function.",
-    "I'm just lost!": "If you still need help or don't understand something in the docs, then you can join the Community and ask a question in the right forums to get extra help."
+    "I'm just lost!": "If you still need help or don't understand something in the docs, then you can join the Community and ask a question in the right forums to get extra help.",
+    "I got some fantastic idea or found a nasty bug, what now?": "If you found a bug or have an idea on how we can improve. \nPlease do not hesitate and join our community and suggest or report your issue/idea so we or a community member can take a look and help you. \nPlease provide as many details as possible, screenshots, console messages, examples etc this way we can implement your issue or suggestion faster."
 }
 
 export { troubleShooting }
