d4ab0ed864449c6665a870ebe86b2573b4cd88a4
Sketch <54947833+lukeplays33@users.noreply.github.com>
2025-01-02 07:15:41 +0000

    test

--- js/index.js
+++ js/index.js
@@ -40,10 +40,6 @@ for (i of items) {
   i.class = '';
 
   i.addEventListener('click', function (e) {
-    if(this.classList.contains('current')) {
-      this.parentNode.toggle.click();
-    }
-    
     if (this.id == 'promo') {
       e.preventDefault();
       e.stopPropagation();
