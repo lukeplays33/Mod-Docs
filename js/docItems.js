fcc1cda42e30662fea6d64addaf08886288776c3
Sketch <54947833+lukeplays33@users.noreply.github.com>
2025-01-03 13:08:04 +0000

    test

--- js/docItems.js
+++ js/docItems.js
@@ -4,10 +4,19 @@ let description = document.getElementById('description');
 let items = document.getElementById('items');
 
 let i;
+let jsonObject = docsItemsJson[window.sessionStorage.getItem('docItem')].items;
 
 description.children[1].innerHTML = docsItemsJson[window.sessionStorage.getItem('docItem')].description;
-items.listItems = Object.keys(docsItemsJson[window.sessionStorage.getItem('docItem')].items);
+items.listItems = Object.keys(jsonObject);
 
+for(i of Object.keys(docsItemsJson[window.sessionStorage.getItem('docItem')].items)) {
+    if(docsItemsJson[window.sessionStorage.getItem('docItem')].items[i] == 'Type: divider') {
+        let hr = document.createElement('hr');
+        hr.setAAttribute('titleText', i);
+
+        document.getElementById(i).replaceWith(hr);
+    }
+}
 
 items.addEventListener('itemSelected', function (e) {
     let name = window.sessionStorage.getItem('docItem') + ' - ' + e.detail.value; // the new document.title
