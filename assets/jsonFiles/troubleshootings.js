e0b57c361b260391bd710085602e038d58966f3f
Sketch <54947833+lukeplays33@users.noreply.github.com>
2025-01-06 10:36:39 +0000

    added a troubleshooting item about exerimental deviceInfo modules

--- assets/jsonFiles/troubleshootings.js
+++ assets/jsonFiles/troubleshootings.js
@@ -1,7 +1,8 @@
 let troubleShooting = {
     "No events captured.": "Ensure that the module is focused so you can capture events, this can be done by using the requestFocus() function.",
     "I'm just lost!": "If you still need help or don't understand something in the docs, then you can join the Community and ask a question in the right forums to get extra help.",
-    "I got some fantastic idea or found a nasty bug, what now?": "If you found a bug or have an idea on how we can improve. \nPlease do not hesitate and join our community and suggest or report your issue/idea so we or a community member can take a look and help you. \nPlease provide as many details as possible, screenshots, console messages, examples etc this way we can implement your issue or suggestion faster."
+    "I got some fantastic idea or found a nasty bug, what now?": "If you found a bug or have an idea on how we can improve. \nPlease do not hesitate and join our community and suggest or report your issue/idea so we or a community member can take a look and help you. \nPlease provide as many details as possible, screenshots, console messages, examples etc this way we can implement your issue or suggestion faster.",
+    "The deviceInfo module does not always return something or wrong values": "This is because the code (navigator and userAgentData) are experimental codes this means that not all devices or browsers have it implemented yet."
 }
 
 export { troubleShooting }
