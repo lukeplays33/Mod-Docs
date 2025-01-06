df089dd60387b1646043ad067535212e9becda6b
Sketch <54947833+lukeplays33@users.noreply.github.com>
2025-01-06 11:13:28 +0000

    test

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,8 @@
 import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
 
-let pichai = new PichaiUX();
+let options = {Homelink: window.location.href}
+
+let pichai = new PichaiUX(options);
 pichai.initialize();
 
 document.body.style.backgroundImage = 'none';
