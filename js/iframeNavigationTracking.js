5b4bf852b6fee6c3a0ffb46536df04888bf885b0
Sketch <54947833+lukeplays33@users.noreply.github.com>
2025-01-10 10:42:50 +0000

    test

--- js/iframeNavigationTracking.js
+++ js/iframeNavigationTracking.js
@@ -20,7 +20,7 @@ function addNewURL() {
 }
 
 function goBackward() {
-    let newTitle = backwardStackURL[backwardStackURL.length - 2];
+    let newTitle = backwardStack[backwardStack.length - 2];
     console.log(newTitle)
 
     forwardStack.push(newTitle);
@@ -33,7 +33,7 @@ function goBackward() {
 }
 
 function goForward() {
-    let newTitle = forwardStack.slice(0)[1];
+    let newTitle = forwardStack[forwardStack.length - 2];
 
     backwardStack.push(currentTitle);
     backwardStackURL.push(currentURL);
