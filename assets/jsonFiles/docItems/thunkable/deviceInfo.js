1d192db603e189dc7a5edb5b1635a4c2df74829e
luke hoogenboom <6035070@mborijnland.nl>
2025-01-21 14:52:58 +0100

    added getCOuntryName to docs

--- assets/jsonFiles/docItems/thunkable/deviceInfo.js
+++ assets/jsonFiles/docItems/thunkable/deviceInfo.js
@@ -450,6 +450,23 @@ let deviceInfo = {
       outputValueOptions: ['string'], // none for events
       type: 'Function', // boolean,int, string, function, class etc or event if it's a event or property for property of elements etc
       displayFile: 'https://i.ibb.co/PDTh0Xy/Screenshot-2025-01-21-143605.png', // the file that is displayed all together with the info, it can be an image, video, a code file or a interactive demo that let's user see what happens when a variable it's value is changed for example.
+    }, 
+
+    "getCountryName": {
+      description: 'The getCountryName function returns the clcountry name of the device this is based on the system language the user or manufacturer has set.',
+      variables: { //the input or outputs it can have, the types they return or accept such as a string or number
+      },
+
+      controls: { // tells the docs page to display controls for displayFile
+        fullscreen: true,
+        play: false,
+        console: false,
+        viewCode: false,
+      },
+
+      outputValueOptions: ['string'], // none for events
+      type: 'Function', // boolean,int, string, function, class etc or event if it's a event or property for property of elements etc
+      displayFile: 'https://i.ibb.co/6FDJNbF/Screenshot-2025-01-21-145213.png', // the file that is displayed all together with the info, it can be an image, video, a code file or a interactive demo that let's user see what happens when a variable it's value is changed for example.
     },
   },
   //quickLinks: [], //e.g. for quickly navigating to a tutorial
