6a05f6037bf8a8177be9424accd52fde9a4812df
luke hoogenboom <6035070@mborijnland.nl>
2025-01-21 14:26:18 +0100

    fixed an issue that could cause recents visited item history to only show last 2 items

--- js/explainItem.js
+++ js/explainItem.js
@@ -271,7 +271,6 @@ function setRecents(name) {
 
         value = [...new Set(value)]; // removes all duplicated items
         value = value.slice(0, 3);
-        console.log(value)
 
         localforage.setItem('recentDocItems', JSON.stringify(value)); // removes all duplicated items and saves them as a recent page
     }).catch(function (err) {
