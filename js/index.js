217850fa08a1949feeaa27777cd72289e52f2bb1
luke hoogenboom <6035070@mborijnland.nl>
2025-01-22 11:32:10 +0100

    fixed imports

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
+import { PichaiUX } from 'https://dreamforge-forging-our-dreams-in-tech.github.io/Pichai-UX/imports.js';
 import { docsItemsJson } from '../assets/jsonFiles/docsItems.js';
 import { tutorialsJson } from '../assets/jsonFiles/tutorials.js';
 
--- js/setup.js
+++ js/setup.js
@@ -1,4 +1,4 @@
-import { PichaiUX } from 'https://lukeplays33.github.io/Pichai-UX/imports.js';
+import { PichaiUX } from 'https://dreamforge-forging-our-dreams-in-tech.github.io/Pichai-UX/imports.js';
 
 let pichai = new PichaiUX();
 pichai.initialize();
