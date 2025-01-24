39b9536110e69c3d4098c72c9b4a912de6a8b0f1
luke hoogenboom <6035070@mborijnland.nl>
2025-01-24 11:16:53 +0100

    fixed header navigation

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,8 @@
 import { PichaiUX } from 'https://dreamforge-forging-our-dreams-in-tech.github.io/Pichai-UX/imports.js';
 
-let pichai = new PichaiUX();
+let options = { homeLink: 'https://lukeplays33.github.io/Mod-Docs/' }
+
+let pichai = new PichaiUX(options);
 pichai.initialize();
 
 document.body.style.backgroundImage = 'none';
