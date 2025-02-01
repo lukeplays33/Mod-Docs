c76d9802ef62de1d10f4732d1850d2a55ae66496
luke hoogenboom <6035070@mborijnland.nl>
2025-02-01 10:52:46 +0100

    added a project remix link so people can look inside of the module playground project nad see its code

--- assets/jsonFiles/docItems/thunkable/DnD.js
+++ assets/jsonFiles/docItems/thunkable/DnD.js
@@ -40,6 +40,7 @@ let DnD = {
 
     demoLinks: { // links to examples users can look at
       'Playground': 'https://x.thunkable.com/web-build/index.html?projectId=675034b7267bc8dd4bc33f6c&noframe=1&responsive=1&screenId=undefined',
+      "Remix sample Project": "https://x.thunkable.com/projectPage/675034b7267bc8dd4bc33f6c",
     },
 
     repo: {
