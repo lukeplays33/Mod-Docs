ec47311c7b53df4fe94becb03597e762255d68e9
Luke-technolab <luke@technolableiden.nl>
2025-06-24 15:19:36 +0200

    test

--- js/setup.js
+++ js/setup.js
@@ -1,6 +1,6 @@
 import { PichaiUX } from 'https://dreamforge-forging-our-dreams-in-tech.github.io/Pichai-UX/imports.js';
 
-let options = { homeLink: 'https://lukeplays33.github.io/Mod-Docs/', }
+let options = { homeLink: 'https://lukeplays33.github.io/Mod-Docs/', hueRotation:0 }
 
 let pichai = new PichaiUX(options);
 pichai.initialize();
